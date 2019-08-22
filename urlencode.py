96474e9a1973ab3ee0d74b630d2960d3b2bf93eb
mikem2314 <mikem2314@gmail.com>
2019-08-22 20:06:01 +0000

    added shebang line to indicate synapse python binary. formatted print more clearly with newlines

--- urlencode.py
+++ urlencode.py
@@ -1,7 +1,9 @@
+#!/home/riotadmin/synapse/env/bin/python
+
 import urllib.parse
 
 stringToDecode = input("Enter URL/string to encode: ")
 
 decodedString = urllib.parse.quote(stringToDecode)
 
-print (decodedString)
+print ("\n Encoded string: \n\n ", decodedString, "\n")
